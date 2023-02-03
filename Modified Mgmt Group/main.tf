5fedf66a811bf465f671362b363eb9a6eada8612
dharrilal <denesh.harrilal@gmail.com>
2023-02-02 23:30:30 -0600

    Update main.tf

--- Modified Mgmt Group/main.tf
+++ Modified Mgmt Group/main.tf
@@ -76,9 +76,4 @@ module "enterprise_scale" {
       }
     }
   }
-
-  deploy_management_resources    = var.deploy_management_resources
-  subscription_id_management     = data.azurerm_client_config.core.subscription_id
-  configure_management_resources = local.configure_management_resources
-
 }

cf1823dc748ab42498fd3ef3e2316c7010cc9b22
dharrilal <denesh.harrilal@gmail.com>
2023-02-02 22:01:50 -0600

    updates

---
+++ Modified Mgmt Group/main.tf
@@ -0,0 +1,66 @@
+# Get the current client configuration from the AzureRM provider.
+# This is used to populate the root_parent_id variable with the
+# current Tenant ID used as the ID for the "Tenant Root Group"
+# Management Group.
+
+data "azurerm_client_config" "core" {}
+
+# Declare the Azure landing zones Terraform module
+# and provide a base configuration.
+
+module "enterprise_scale" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = "3.1.2"
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.connectivity = azurerm
+    azurerm.management   = azurerm
+  }
+
+  root_parent_id = data.azurerm_client_config.core.tenant_id
+  root_id        = var.root_id
+  root_name      = var.root_name
+  library_path   = "${path.root}/lib"
+
+  custom_landing_zones = {
+    "${var.root_id}-LandingZoneProd" = {
+      display_name               = "${upper(var.root_id)} LandingZoneProd"
+      parent_management_group_id = "${var.root_id}-landing-zones"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "customer_online"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+    "${var.root_id}-LandingZoneQA" = {
+      display_name               = "${upper(var.root_id)} LandingZoneQA"
+      parent_management_group_id = "${var.root_id}-landing-zones"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "customer_online"
+        parameters     = {}
+        access_control = {}
+      }  
+    }
+    "${var.root_id}-LandingZoneDev" = {
+      display_name               = "${upper(var.root_id)} LandingZoneDev"
+      parent_management_group_id = "${var.root_id}-landing-zones"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "customer_online"
+        parameters     = {
+          Deny-Resource-Locations = {
+            listOfAllowedLocations = ["canadacentral","canadaeast",]
+          }
+          Deny-RSG-Locations = {
+            listOfAllowedLocations = ["canadacentral","canadaeast",]
+          }
+        }
+        access_control = {}
+      }
+    }
+  }
+
+}
---
+++ Modified Mgmt Group/variables.tf
@@ -0,0 +1,11 @@
+# Use variables to customize the deployment
+
+variable "root_id" {
+  type    = string
+  default = "OAGAlberta"
+}
+
+variable "root_name" {
+  type    = string
+  default = "OAG Alberta"
+}

bf5661a892003976bc89121a7a45d2dcea19a403
dharrilal <denesh.harrilal@gmail.com>
2023-02-12 12:55:39 -0400

    Update main.tf

--- Modified Mgmt Group/IndividulalModules/Management/main.tf
+++ Modified Mgmt Group/IndividulalModules/Management/main.tf
@@ -1,29 +1,67 @@
-# Get the current client configuration from the AzureRM provider.
-# This is used to populate the root_parent_id variable with the
-# current Tenant ID used as the ID for the "Tenant Root Group"
-# Management Group.
+# Declare a standard provider block using your preferred configuration.
+# This will target the "default" Subscription and be used for the deployment of all "Core resources".
+provider "azurerm" {
+  features {}
+}
 
-data "azurerm_client_config" "core" {}
+# Declare an aliased provider block using your preferred configuration.
+# This will be used for the deployment of all "Connectivity resources" to the specified `subscription_id`.
+provider "azurerm" {
+  alias           = "connectivity"
+  subscription_id = "07e2424a-029a-4ae7-b114-d91306b23f5b"
+  features {}
+}
 
-# Declare the Azure landing zones Terraform module
-# and provide a base configuration.
+# Declare a standard provider block using your preferred configuration.
+# This will be used for the deployment of all "Management resources" to the specified `subscription_id`.
+provider "azurerm" {
+  alias           = "management"
+  subscription_id = "4dfb599e-8459-4600-86a9-a19faa01ad6d"
+  features {}
+}
 
+# Obtain client configuration from the un-aliased provider
+data "azurerm_client_config" "core" {
+  provider = azurerm
+}
+
+# Obtain client configuration from the "management" provider
+data "azurerm_client_config" "management" {
+  provider = azurerm.management
+}
+
+# Obtain client configuration from the "connectivity" provider
+data "azurerm_client_config" "connectivity" {
+  provider = azurerm.connectivity
+}
+
+# Map each module provider to their corresponding `azurerm` provider using the providers input object
 module "enterprise_scale" {
   source  = "Azure/caf-enterprise-scale/azurerm"
   version = "3.1.2"
 
   providers = {
     azurerm              = azurerm
-    azurerm.connectivity = azurerm
-    azurerm.management   = azurerm
+    azurerm.management   = azurerm.management
+    azurerm.connectivity = azurerm.connectivity
   }
 
-  root_parent_id = data.azurerm_client_config.core.tenant_id
-  root_id        = var.root_id
-  root_name      = var.root_name
+  # Set the required input variable `root_parent_id` using the Tenant ID from the un-aliased provider
+  root_parent_id           = data.azurerm_client_config.core.tenant_id
 
-  deploy_management_resources    = var.deploy_management_resources
-  subscription_id_management     = data.azurerm_client_config.core.subscription_id
-  configure_management_resources = local.configure_management_resources
+  # Disable creation of the core management group hierarchy
+  deploy_core_landing_zones = false
+
+  # Enable deployment of the management resources, using the management
+  # aliased provider to populate the correct Subscription ID
+  deploy_management_resources    = true
+  subscription_id_management     = data.azurerm_client_config.management.subscription_id
+
+  # Enable deployment of the connectivity resources, using the connectivity
+  # aliased provider to populate the correct Subscription ID
+  deploy_connectivity_resources    = False
+  subscription_id_connectivity     = data.azurerm_client_config.connectivity.subscription_id
+
+  # insert additional optional input variables here
 
 }

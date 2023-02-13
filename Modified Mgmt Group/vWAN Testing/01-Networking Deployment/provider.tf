c68c86429830f788779441b39d544ba195e18cbf
dharrilal <denesh.harrilal@gmail.com>
2023-02-13 17:46:58 -0400

    Update provider.tf

--- Modified Mgmt Group/vWAN Testing/01-Networking Deployment/provider.tf
+++ Modified Mgmt Group/vWAN Testing/01-Networking Deployment/provider.tf
@@ -5,7 +5,6 @@ terraform {
       # Specify what version of the provider we are going to utilise
       source  = "hashicorp/azurerm"
       version = ">= 3.41.0"
-      subscription_id = var.subscription_id
     }
     random = {
       source  = "hashicorp/random"
